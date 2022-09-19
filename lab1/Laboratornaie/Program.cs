8f93b72e3d509c3c25e6657e6b336cbf09dd2ce1
Vladislav <limboexe926@gmail.com>
2022-09-19 16:54:27 +0300

    comlete

---
+++ lab1/Laboratornaie/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace lab1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Task1();
+            Console.WriteLine();
+            Task2();
+            Console.WriteLine();
+            Task3();
+            Console.WriteLine();
+            Task4();
+        }
+        static void Task1()
+        {
+            Console.WriteLine("Первое задание:");
+            double first = (Math.Pow(2, 2) - Math.Pow(3, 3));
+            double second = (Math.Sqrt(Math.Pow(3, 4) - Math.Pow(2, 4)));
+            double third = first / second;
+            double fourth = Math.Abs(Math.Pow(3, 5) - Math.Pow(15, 2.5));
+            double fifth = third + fourth;
+            double sixth = Math.Sqrt(fifth);
+            Console.WriteLine("Ответ: " + sixth);
+        }
+        static void Task2()
+        {
+            Console.WriteLine("Второе задание:");
+            Random rnd = new Random();
+            int a = rnd.Next(-100, 100);
+            Console.WriteLine("a=" + a);
+            int b = rnd.Next(-100, 100);
+            Console.WriteLine("b=" + b);
+            double c = (Math.Pow(a, 2) + Math.Cbrt(b)) / (a + b);
+            Console.WriteLine("c=" + c);
+        }
+        static void Task3()
+        {
+            Console.WriteLine("Третье задание:");
+            double a = 5.93576;
+            int b = (int)a;
+            Console.WriteLine("b=" + b);
+            double c = Convert.ToInt32(Math.Round(a));
+            Console.WriteLine("c=" + c);
+            double d = (Math.Floor(a));
+            Console.WriteLine("d=" + d);
+            double e = Math.Round(a, 1);
+            Console.WriteLine("e=" + e);
+            int f = 5;
+            int g = f++;
+            Console.WriteLine("g=" + g);
+            int F = 5;
+            int G = ++F;
+            Console.WriteLine("G=" + G);
+        }
+        static void Task4()
+        {
+            Console.WriteLine("Четвертое задание:");
+            Random rnd = new Random();
+            double chislo = rnd.Next(-100, 100) + rnd.NextDouble();
+            chislo = Math.Round(chislo, 3);
+            Console.WriteLine("Рандомное десятичное число от -100 до 100=" + chislo);
+        }
+    }
+}
+
---
+++ lab1/Laboratornaya2/Program.cs
@@ -0,0 +1,178 @@
+﻿using System;
+
+namespace lab2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Task1();
+            Console.WriteLine();
+            Task2();
+            Console.WriteLine();
+            Task3();
+            Console.WriteLine();
+        }
+        static void Task1()
+        {
+            Console.WriteLine("Задание 1");
+
+            Console.WriteLine("Введите число в диапазоне [1,7]");
+            string strNumber = Console.ReadLine();
+            bool isNumber = int.TryParse(strNumber, out int number);
+            if (number > 1 && number <= 7)
+            {
+                string day = "";
+                switch (number)
+                {
+                    case 1:
+
+                        day = "Понедельник";
+
+                        break;
+
+                    case 2:
+
+                        day = "Вторник";
+
+                        break;
+
+                    case 3:
+
+                        day = "Среда";
+
+                        break;
+
+                    case 4:
+
+                        day = "Четверг";
+
+                        break;
+
+                    case 5:
+
+                        day = "Пятница";
+
+                        break;
+
+                    case 6:
+
+                        day = "Суббота";
+
+                        break;
+
+                    case 7:
+
+                        day = "Воскресенье";
+
+                        break;
+
+                    default:
+
+                        Console.WriteLine("Вы ввели число больше заданного диапазона");
+
+                        break;
+                }
+                Console.WriteLine(day);
+            }
+
+            string ifDay = "";
+            if (number > 1 && number <= 7)
+            {
+                if (number == 1)
+                {
+                    ifDay = "Понедельник";
+                }
+
+                if (number == 2)
+                {
+                    ifDay = "Вторник";
+                }
+
+                if (number == 3)
+                {
+                    ifDay = "Среда";
+                }
+
+                if (number == 4)
+                {
+                    ifDay = "Четверг";
+                }
+
+                if (number == 5)
+                {
+                    ifDay = "Пятница";
+                }
+
+                if (number == 6)
+                {
+                    ifDay = "Суббота";
+                }
+
+                if (number == 7)
+                {
+                    ifDay = "Воскресенье";
+                }
+            }
+            Console.WriteLine(ifDay);
+        }
+        static void Task2()
+        {
+            Console.WriteLine("Задание 2");
+            int fiznumber = int.Parse(Console.ReadLine());
+
+            if (fiznumber % 3 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+
+            else if (fiznumber % 5 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+
+            if (fiznumber % 3 == 0 && fiznumber % 5 == 0)
+            {
+                Console.WriteLine("BuzzFizz");
+            }
+        }
+        static void Task3()
+        {
+            Console.WriteLine("Задание 3");
+            double Value_1;
+            double Value_2;
+            string action;
+
+            Console.Write("Введите число 1: ");
+            Value_1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Введите число 2: ");
+            Value_2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Выберите операцию '+', '-', '*', '/' ");
+            action = Console.ReadLine();
+
+            switch (action)
+            {
+                case "+":
+                    Console.WriteLine(Value_1 + Value_2);
+                    break;
+                case "-":
+                    Console.WriteLine(Value_1 - Value_2);
+                    break;
+                case "*":
+                    Console.WriteLine(Value_1 * Value_2);
+                    break;
+                case "/":
+                    Console.WriteLine(Value_1 / Value_2);
+                    break;
+                default:
+                    {
+                        Console.WriteLine("Неверное значение!");
+                    }
+                    break;
+            }
+        }
+    }
+}
+
